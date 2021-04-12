fb040ef7d07dd13c3e5474ec28a36c5c07d76de2
ash-miku <29676159+ash-miku@users.noreply.github.com>
2021-04-12 18:05:32 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -225,12 +225,10 @@ class Notify(object):
             }
             if Notify.DD_BOT_SECRET != '':
                 secret = Notify.DD_BOT_SECRET
-                print(secret)
                 timestamp = int(round(time.time() * 1000))
                 secret_enc = secret.encode('utf-8')
-                print(secret_enc)
                 string_to_sign = '{}\n{}'.format(timestamp, secret)
-                string_to_sign_enc = bytes(string_to_sign).encode('utf-8')
+                string_to_sign_enc = string_to_sign.encode('utf-8')
                 hmac_code = hmac.new(
                     secret_enc, string_to_sign_enc,
                     digestmod=hashlib.sha256).digest()
